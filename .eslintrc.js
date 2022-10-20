405f84e96d86861bca0c11cfe07b48d6ed58736b
Maryam Rahimi <mrahimi.jp@gmail.com>
2022-10-21 00:43:12 +0330

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,26 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    parserOptions: {
-        parser: 'babel-eslint',
-    },
-    extends: [
-        "plugin:vue/essential",
-        "plugin:prettier/recommended",
-        "eslint:recommended"
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: ['plugin:vue/essential', 'plugin:prettier/recommended', 'eslint:recommended'],
+  plugins: [],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        printWidth: 130,
+        singleQuote: true,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+        tabWidth: 2,
+      },
     ],
-    plugins: [],
-    rules: {
-        'vue/multi-word-component-names': 'off',
-        'prettier/prettier': [
-            'error',
-            {
-                semi: true,
-                printWidth: 130,
-                singleQuote: true,
-                trailingComma: 'all',
-                endOfLine: 'auto',
-                tabWidth: 2,
-            },
-        ],
-    },
+  },
 };
